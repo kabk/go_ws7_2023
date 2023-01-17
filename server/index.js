e35ba2ae95963ac089504de811690abf96bdeb58
James Bryan Graves <jbg@Macintosh.local>
2023-01-17 13:13:38 +0100

    server writes persistant files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
-  fs = require('fs')
+  fs = require('fs').promises
 
 const app = express()
 const port = 3000
@@ -9,34 +9,37 @@ const port = 3000
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
 app.use(bodyParser.json({ limit: '50mb' }))
 
-function readData(userId) {
-  fs.readFile(`./${userId}.txt`, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    return parseInt(userId)
-  })
+async function readData(userId) {
+  try {
+    const val = await fs.readFile(`./${userId}.txt`, 'utf8')
+    return parseInt(val)
+  } catch(err){
+    console.log(err)
+    return 0
+  }
 }
 
-function writeData(userId, val) {
-  fs.writeFile(`./${userId}.txt`, val, err => {
-    if (err) {
-      console.error(err)
-    }
-  })
+async function writeData(userId, val) {
+  try {
+    await fs.writeFile(`./${userId}.txt`, `${val}`)
+  } catch(err) {
+    console.log(err)
+  }
 }
 
 // I handle GET requests - JBG
-app.get('/', (req, res) => {
-  res.send(JSON.stringify({ 'msg': 'Hello world!', 'val': 2 }))
+app.get('/', async (req, res) => {
+  const userId = req.query.userId
+  res.send(JSON.stringify({ 'val':  await readData(userId) }))
 })
 
 // I handle POST requests - JBG
 app.post('/', async (req, res) => {
   try {
-    console.log('Client sent: ', req.body.userId)
-    console.log('Client sent: ', req.body.val)
+    let userId = req.body.userId
+    let val = await readData(userId)
+    val += 1
+    await writeData(userId, `${val}`)
     res.send('POST request handled')
   } catch(e) {
     res.status(500).send(e)
